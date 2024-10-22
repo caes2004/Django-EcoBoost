5eeef61d67efba2fc81a521d7dc3f3d40426bf9e
escaes <esteban_cano23232@elpoli.edu.co>
2024-10-22 18:45:23 -0500

    Interfaz de Home y cambios de headers en la ventana de Admin.

--- aplicaciones/Crud_admin/admin.py
+++ aplicaciones/Crud_admin/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from.models import User
 from .models import Producto
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
 admin.site.register(User)
 
 admin.site.register(Producto)
+
+# Cambiar el título del sitio
+admin.site.site_header = _("Admin EcoBoost")
+
+# Cambiar el título que aparece en la ventana del navegador
+admin.site.site_title = _(" Admin")
+
+# Cambiar el mensaje de bienvenida en la página de inicio del admin
+admin.site.index_title = _("Bienvenido a la administración de Ecoboost")
